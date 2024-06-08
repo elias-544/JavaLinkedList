a929488fed8d6efde79a2f2f4606cd71b12cf943
elias <eliaskhatoun@hotmail.com>
2024-06-08 00:13:15 -0400

    Changes

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.elias","l":"JavaLinkedList"},{"p":"com.elias","l":"JavaLinkedList.Node"}];updateSearchResults();
